2f3208d0dd69be1865cb66e74595933b3eb4de27
aadi <aadi@aadi.net.in>
2024-02-18 21:25:35 +0530

    getFile fix

--- main.sh
+++ main.sh
@@ -41,7 +41,7 @@ downloader () {
  fileId=$(grep -w ${key} ${db} | awk '{print $2}')
  echo "file id for ${key} is ${fileId} "
  
- pathFile=$(curl -s "${apiEnd}?file_id=${fileId}" | grep -o '"file_path":"[^"]*' | awk -F'"' '{print $4}')
+ pathFile=$(curl -s "${apiEnd}/getFile?file_id=${fileId}" | grep -o '"file_path":"[^"]*' | awk -F'"' '{print $4}')
  dlURL="https://api.telegram.org/file/bot${TOKEN}/${pathFile}"
 
  curl -o ${key} ${dlURL}
